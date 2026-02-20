f9d459ff65a9d6c66cf76d828f3c9c2e7e490aa1
tristan368 <tristan@haraldhouse.com>
2026-02-20 12:17:40 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -334,7 +334,7 @@ import NetlifyForm from "./components/NetlifyForm.vue";
     <!-- Image Column -->
     <div class="w-full md:w-1/2 flex">
      <img
-  src="./assets/Untitled_1.1.13.png"
+  src="./assets/untitled_1.1.13.png"
   alt="Epilogues still"
   class="w-full h-auto"
 />
